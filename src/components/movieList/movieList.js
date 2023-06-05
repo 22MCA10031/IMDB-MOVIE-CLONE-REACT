953aa953f8ce31e8d261f2ad2b6cf0a2199f7d70
Adarsh Rajan <adarshrajan242@gmail.com>
2023-06-05 23:51:47 +0530

    second

--- src/components/movieList/movieList.js
+++ src/components/movieList/movieList.js
@@ -1,39 +1,67 @@
-import React, {useEffect, useState} from "react"
-import "./movieList.css"
-import { useParams } from "react-router-dom"
-import Cards from "../card/card"
+import React, { useEffect, useState } from "react";
+import "./movieList.css";
+import { useParams, useLocation, useNavigate } from "react-router-dom";
+import Cards from "../card/card";
 
 const MovieList = () => {
-    
-    const [movieList, setMovieList] = useState([])
-    const {type} = useParams()
+  const [movieList, setMovieList] = useState([]);
+  const { type } = useParams();
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const searchQuery = queryParams.get("q");
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        getData()
-    }, [])
+  useEffect(() => {
+    getData();
+  }, [type, searchQuery]);
 
-    useEffect(() => {
-        getData()
-    }, [type])
-
-    const getData = () => {
-        fetch(`https://api.themoviedb.org/3/movie/${type ? type : "popular"}?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`)
-        .then(res => res.json())
-        .then(data => setMovieList(data.results))
+  const getData = () => {
+    let apiUrl = `https://api.themoviedb.org/3/movie/${type ? type : "popular"}?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`;
+    if (searchQuery) {
+      apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US&query=${searchQuery}`;
     }
 
-    return (
-        <div className="movie__list">
-            <h2 className="list__title">{(type ? type : "POPULAR").toUpperCase()}</h2>
-            <div className="list__cards">
-                {
-                    movieList.map(movie => (
-                        <Cards movie={movie} />
-                    ))
-                }
-            </div>
-        </div>
-    )
-}
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((data) => setMovieList(data.results || []))
+      .catch((error) => {
+        console.error("Error fetching movie data:", error);
+        setMovieList([]);
+      });
+  };
 
-export default MovieList
+  const handleSearch = (e) => {
+    e.preventDefault();
+    const query = e.target.search.value;
+    if (query.trim() !== "") {
+      setMovieList([]);
+      navigate(`/movies/search?q=${query}`);
+    }
+  };
+
+  return (
+    <div className="movie__list">
+        <div className="search__form">
+        <form onSubmit={handleSearch}>
+          <input
+            type="text"
+            name="search"
+            placeholder="Search movies"
+            className="search__input"
+          />
+          <button type="submit" className="search__button">
+            Search
+          </button>
+        </form>
+      <h2 className="list__title">{(type ? type : "POPULAR").toUpperCase()}</h2>
+      </div>
+      <div className="list__cards">
+        {movieList.map((movie) => (
+          <Cards movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MovieList;
